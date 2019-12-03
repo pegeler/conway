39c3f561b17c334e824e6d5acb57a3e6f0dd493b
pegeler <32426645+pegeler@users.noreply.github.com>
2019-12-03 02:42:47 -0500

    Conway's game of life in R

---
+++ life.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Conway's Game of Life
+
+Rules
+------------------------------------------------------
+1. Any live cell with two or three neighbors survives.
+2. Any dead cell with three live neighbors becomes a live cell.
+3. All other live cells die in the next generation. 
+   Similarly, all other dead cells stay dead.
+
+Reference
+------------------------------------------------------
+https://en.wikipedia.org/wiki/Conway's_Game_of_Life#Rules
+"""
+
+def life(state):
+    pass # Coming soon?
+
